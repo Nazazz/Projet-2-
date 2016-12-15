d2357ffdfe3a0dddec2696eb838d51a75a5bb19b
Paulbgt <paul.boogaerts@viacesi.fr>
2016-12-15 11:36:26 +0100

    Delete fonction.h

--- fonction.h
+++
@@ -1,8 +0,0 @@
-#include "fonction.c"
-
-int tailleImageX(FILE* image);
-int tailleImageY(FILE* image);
-int** inserTabPbm (FILE* image,int tabx, int taby);
-int calcCentrex(int tabx);
-int calcCentrey(int taby);
-void decaler(int a, int b , int tabAff[23][80]);
